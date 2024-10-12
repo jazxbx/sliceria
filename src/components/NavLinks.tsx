4de5014152552b135504ea1f502cb32a8d7e8e91
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-10-11 22:39:19 -0700

    fix hover color

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -5,17 +5,17 @@ export default function NavLinks() {
     <nav className=''>
       <ul className='flex flex-col justify-evenly gap-8 md:flex-row'>
         <li>
-          <a href='/about' className='hover:text-yellow-400'>
+          <a href='/about' className='hover:text-red-900'>
             menu
           </a>
         </li>
         <li>
-          <a href='/menu' className='hover:text-yellow-400'>
+          <a href='/menu' className='hover:text-red-900'>
             our story
           </a>
         </li>
         <li>
-          <a href='/menu' className='hover:text-yellow-400'>
+          <a href='/menu' className='hover:text-red-900'>
             contact us
           </a>
         </li>
