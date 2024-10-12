b19c89bdd0ecf5ad4482c5ef78939d2589a3e304
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-10-11 18:58:38 -0700

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './layout/Footer';
+import Home from './components/Home';
+import Header from './layout/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
