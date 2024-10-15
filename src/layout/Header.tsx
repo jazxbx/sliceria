9c89b7c3275284e436b34b1205317e24ce085fd2
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-10-14 21:03:36 -0700

    Refactor Header component to update color variable

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,7 +3,7 @@ import Nav from '../components/Nav';
 
 export default function Header() {
   return (
-    <nav className='sticky top-0 w-full z-20 flex-wrap px-7 py-5 flex bg-yellow-400 text-xl font-bold mx-auto justify-between'>
+    <nav className='sticky top-0 w-full z-20 flex-wrap px-7 py-5 flex bg-yellow text-brown text-xl font-bold mx-auto justify-between'>
       <Logo />
       <Nav />
     </nav>
