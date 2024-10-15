ade44ecd8243e157d534c9df6879f92b0d984b08
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-10-14 21:04:20 -0700

    Add OurStory page to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css';
 import Footer from './layout/Footer';
 import Home from './pages/Home';
 import Header from './layout/Header';
+import OurStory from './pages/OurStory';
 
 function App() {
   return (
     <>
+      <OurStory />
       <Header />
       <Home />
       <Footer />
