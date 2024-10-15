ce0ea8a366651d9db4a409113cc8e73a02778e48
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-10-14 21:02:41 -0700

    update: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: '#FACC14',
+        orange: '#D55E06',
+        brown: '#251804',
+        red: '#EB2C1E',
+        cream: '#F9EDCD',
+        green: '#8D8E6A',
+      },
+    },
   },
   plugins: [],
 };
