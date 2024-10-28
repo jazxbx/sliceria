edc59ff396c5f4fd5972024669df578b1bb622ab
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-10-28 15:09:32 -0700

    Refactor Card component to remove unused code and update styling

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,23 +1,44 @@
-import pizza from '../assets/food-photographer-Ew_9G3UfsA0-unsplash.jpg';
-import Card from './Card';
+import pizza from '../assets/aldward-castillo-p1SBJANd0Q8-unsplash-removebg-preview.png';
+import Card from '../components/Card';
 
 export default function Menu() {
   // our daily bread - regular pizza
   // vegan profecy - vegan pizza
   // gluten-free gospel - gluten free pizza
   return (
-    <div className='bg-cream px-5 pt-9'>
-      <p className='font-extrabold text-2xl'>Our Daily Bread</p>
+    <div className='bg-white px-5 pt-9'>
+      <h1 className='text-5xl md:text-7xl font-bold'>MENU</h1>
       <p className='font-semibold'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero asperiores
         quas!
       </p>
-      <Card
-        src={pizza}
-        alt='pizza'
-        title='Pizza Ipsum'
-        desc='Meatball fresh tomatoes ham string cheese broccoli. Extra sauce bianca white garlic NY style sausage chicken and bacon pepperoni bbq party large white pizza sauteed onions.'
-      />
+      {/* Container */}
+      <div className='grid md:grid-cols-3 lg:grid-cols-4'>
+        <Card
+          src={pizza}
+          alt='pizza'
+          title='Pizza Ipsum'
+          desc='Meatball fresh tomatoes ham string cheese broccoli.'
+        />
+        <Card
+          src={pizza}
+          alt='pizza'
+          title='Pizza Ipsum'
+          desc='Meatball fresh tomatoes ham string cheese broccoli.'
+        />
+        <Card
+          src={pizza}
+          alt='pizza'
+          title='Pizza Ipsum'
+          desc='Meatball fresh tomatoes ham string cheese broccoli.'
+        />
+        <Card
+          src={pizza}
+          alt='pizza'
+          title='Pizza Ipsum'
+          desc='Meatball fresh tomatoes ham string cheese broccoli.'
+        />
+      </div>
     </div>
   );
 }
