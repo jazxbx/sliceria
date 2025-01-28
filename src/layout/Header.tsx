9ee1ba7844b8b1b683b502be515c7256abd664c4
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-28 00:41:52 -0800

    Update header component styling

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,7 +3,7 @@ import Nav from '../components/Nav';
 
 export default function Header() {
   return (
-    <nav className='sticky top-0 w-full z-20 flex-wrap px-7 py-5 flex bg-brown text-cream text-xl font-bold mx-auto justify-between'>
+    <nav className='sticky top-0 w-full z-20 flex-wrap px-10 py-6 flex bg-brown text-cream text-xl font-bold mx-auto justify-between'>
       <Logo />
       <Nav />
     </nav>
