4619e3100300258afb71d3331383d4f2a4b4ea23
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-28 23:55:44 -0800

    uodate about section css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,32 @@
-import hero from '../assets/hero.jpg';
-import about from '../assets/about2.jpg';
+import video from '../assets/hero-vid.mp4';
+import pizza from '../assets/about2.jpg';
 import about2 from '../assets/meg-jenson-d0SJnuNNeIM-unsplash.jpg';
 
 // Hero Section component
-const HeroSection = ({
-  title,
-  imgSrc,
-  altText,
-}: {
-  title: string;
-  imgSrc: string;
-  altText: string;
-}) => (
+const HeroSection = ({ video }: { video: string }) => (
   <section
     id='hero'
-    className='relative inline-block md:h-dvh w-full overflow-hidden'
+    className='relative h-screen flex justify-center text-center items-center'
   >
-    <div className='absolute flex items-center h-full w-full text-center'>
-      <h1 className='inline-block text-yellow text-shadow mx-8 text-3xl md:text-6xl md:text-[5rem] font-bold'>
-        {title}
-      </h1>
-    </div>
-    <img
-      className='h-full w-full object-cover block'
-      src={imgSrc}
-      alt={altText}
-    />
+    <video
+      muted
+      autoPlay
+      loop
+      playsInline
+      className='absolute object-cover top-0 left-0 w-screen h-screen '
+    >
+      <source src={video} type='video/mp4' />
+    </video>
+    <p className='z-10 text-cream font-bold text-[65px] top-4'>
+      slice into <span className='text-yellow'>happiness</span>
+    </p>
   </section>
 );
 
 // Text Section component
 const TextSection = ({ text }: { text: string }) => (
-  <section id='text' className='bg-brown'>
-    <p className='text-cream text-xl md:text-3xl p-10 text-center italic'>
+  <section id='text' className='bg-yellow'>
+    <p className='text-brown text-xl md:text-4xl p-10 text-center uppercase text-wrap'>
       {text}
     </p>
   </section>
@@ -52,60 +46,41 @@ const AboutSection = ({
 }) => (
   <section
     id='about'
-    className='md:p-20 bg-orange flex flex-col md:flex-row pt-20'
+    className=' bg-orange flex flex-col md:flex-row gap-5 px-7 md:px-48 py-14 text-cream '
   >
-    <div className='flex-1 relative'>
-      {/* desktop heading overlay on image */}
-      <div className='absolute z-10 left-[90%] w-[600px] text-balance'>
-        <p className='md:text-7xl leading-7 md:text-yellow md:font-extrabold xl:block hidden'>
-          {heading}
-        </p>
-      </div>
-      <div className='md:my-6 md:ml-4 relative'>
-        <img
-          className='h-[900px] object-cover w-full md:h-full rotate-180 border-2 border-black'
-          src={imgSrc}
-          alt={imgAlt}
-        />
-      </div>
+    {/* right */}
+    {/* TODO: where to add border? which div */}
+    <div className='hidden md:block'>
+      <img className='rotate-180' src={imgSrc} alt='' />
     </div>
-
-    <div className='flex-1 p-14 md:px-5 flex flex-col gap-5 text-center items-center lg:justify-evenly'>
-      {/* mobile heading */}
-      <p className='text-3xl md:text-5xl xl:hidden lg:text-5xl font-bold text-yellow text-left'>
-        {heading}
-      </p>
-      <p className='md:mt-5 text-lg md:text-xl md:leading-loose text-[#FFF0D9] lg:text-2xl'>
-        {description}
-      </p>
-      <div className='border-2 border-black'>
-        <img
-          src={about2}
-          alt='pizza'
-          className='md:w-[400px] md:h-[250px] lg:w-[500px] object-cover'
-        />
+    {/* left */}
+    <div className='flex flex-col gap-7 items-center md:mt-7'>
+      <h2 className='uppercase text-6xl relative right-16'>{heading}</h2>
+      <p className='text-2xl md:text-xl'>{description}</p>
+      {/* picture here */}
+      <div
+        className='max-w-56 w-full border-2 border-black
+        '
+      >
+        <img className='' src={imgSrc} alt='' />
       </div>
     </div>
   </section>
 );
 
 export default function Home() {
-  const heroTitle = 'Where every slice is a divine intervention';
   const aboutDescription = `
-    Gouda mornin'! Cheddar you believe it? Mozzarella a tale as old as thyme. Swiss you were here to brie-lieve it! Fromage all these
-    years, life's been grate, but sometimes it's nacho day. In queso emergency, break out the parmesan to grater good. Ricotta admit,
-    we all have a fet-ta love for cheese, whether it's sharp like cheddar or smooth like camembert. Don’t feel bleu – the world is your
-    charcuterie board, so curdle your worries and embrace the dairy!
+   At Sliceria, we believe that pizza is more than just food – it’s an experience. Every slice is handcrafted with the finest ingredients, from our perfectly crisp crust to our rich, flavorful sauces and premium toppings. Whether you’re craving a classic favorite or a bold new creation, our menu is designed to satisfy every pizza lover. With a commitment to quality, freshness, and community, Sliceria is where friends and family come together to share great moments over delicious slices. Come taste the passion, one slice at a time!
   `;
 
   return (
     <main>
-      <HeroSection title={heroTitle} imgSrc={hero} altText='Pizza Hero Image' />
-      <TextSection text='"No need to get crusty about it, but this pizza’s got some serious cheddar charm."' />
+      <HeroSection video={video} />
+      <TextSection text='Pizza is always the answer ☻' />
       <AboutSection
-        imgSrc={about}
+        imgSrc={pizza}
         imgAlt='Cheesus Crust'
-        heading='Welcome to Cheesus Crust'
+        heading='pizza — it’s good mood food'
         description={aboutDescription}
       />
     </main>
