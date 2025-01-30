53fd4578f71467c35ee39a983bd0a70d6cebd16a
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-29 23:33:13 -0800

    refactor NavLinks component to use ROUTES array

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,18 +1,20 @@
 import { Link } from '@tanstack/react-router';
 
+const ROUTES = [
+  { path: 'menu', name: 'Menu' },
+  { path: 'about', name: 'About Us' },
+  { path: 'contact', name: 'Contact' },
+];
+
 export default function NavLinks() {
   return (
     <nav className=''>
-      <ul className='flex flex-col justify-evenly gap-8 md:flex-row'>
-        <Link href='/menu' className='hover:text-yellow'>
-          menu
-        </Link>
-        <Link href='/about' className='hover:text-yellow'>
-          our story
-        </Link>
-        <Link href='/contact' className='hover:text-yellow'>
-          contact us
-        </Link>
+      <ul className='flex flex-col justify-evenly gap-8 md:flex-row items-center'>
+        {ROUTES.map((route, i) => (
+          <Link key={i} href={'/' + route.path} className='hover:text-yellow'>
+            {route.name}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
