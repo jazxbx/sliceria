8bf8f15a8efdbe6a31530bef1a98d291bf745dce
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-30 00:57:24 -0800

    remove commented-out TanStackRouterDevtools from root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 
@@ -9,7 +9,7 @@ export const Route = createRootRoute({
       <Header />
       <Outlet />
       <Footer />
-      <TanStackRouterDevtools />
+      {/* <TanStackRouterDevtools /> */}
     </>
   ),
 });
