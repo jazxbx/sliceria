0f6732effc9da3962695f5b20cc7397d1dad7067
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-30 00:56:48 -0800

    refactor Card component to include name and price props, and update styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,29 @@
+// TODO: Add alt to src
+
 export default function Card({
   src,
+  name,
   alt,
-  title,
   desc,
+  price,
 }: {
   src: string;
+  name: string;
   alt: string;
-  title: string;
   desc: string;
+  price: number;
 }) {
   return (
-    <div className='rounded-xl shadow-lg'>
+    <div className='border-2 border-black shadow-lg bg-white text-center'>
       <div className='p-5 flex flex-col'>
         <div className='overflow-hidden'>
           <img src={src} alt={alt} className='w-full object-scale-down' />
         </div>
         <h3 className='text-2xl md:text-3xl font-semibold mt-2 tracking-wider'>
-          {title}
+          {name}
         </h3>
         <p className='text-slate-600 mt-3'>{desc}</p>
+        <p>{price}</p>
       </div>
     </div>
   );
