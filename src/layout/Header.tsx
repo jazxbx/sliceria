587a56d15364301695235e22cb2e8b7912d4d5a3
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-30 00:57:10 -0800

    add order button to Header component and update layout

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,17 @@
 import Logo from '../components/Logo';
 import Nav from '../components/Nav';
+// TODO: add route when button clicked
 
 export default function Header() {
   return (
     <nav className='sticky top-0 w-full z-20 flex-wrap px-10 py-6 flex bg-brown text-cream text-xl font-bold mx-auto justify-between'>
       <Logo />
-      <Nav />
+      <div className='flex items-center'>
+        <button className='bg-yellow text-brown py-2 px-4 cursor-pointer rounded-xl mr-6 hover:bg-orange'>
+          order here
+        </button>
+        <Nav />
+      </div>
     </nav>
   );
 }
