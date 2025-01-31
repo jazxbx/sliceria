c14fc60414cf4528b91bfee07f6205165aa6e2bc
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-30 23:12:50 -0800

    add poster img in case video doesnt load

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@tanstack/react-router';
 import Logo from '../components/Logo';
 import Nav from '../components/Nav';
 
@@ -6,12 +5,6 @@ export default function Header() {
   return (
     <nav className='sticky top-0 w-full z-20 flex-wrap px-10 py-6 flex bg-brown text-cream text-xl font-bold mx-auto justify-between items-center'>
       <Logo />
-      <div className='ml-auto'>
-        <button className='bg-yellow text-brown py-2 px-4 cursor-pointer rounded-xl mr-6 hover:bg-orange'>
-          <Link href='/order'>order here</Link>
-        </button>
-      </div>
-
       <Nav />
     </nav>
   );
