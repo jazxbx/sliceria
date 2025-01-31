d6d5b9d05207a3b963f46ae4908e26472270b39e
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-30 19:47:01 -0800

    add noroutefound component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { Link, RouterProvider, createRouter } from '@tanstack/react-router';
 import './index.css';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => (
+    <div className='h-screen bg-yellow flex justify-center items-center'>
+      <div className='flex flex-col items-center text-xl gap-3'>
+        <h1 className='text-3xl'>Sorry, this page doesn't exist</h1>
+        <Link href='/'>let's get back to getting 🍕</Link>
+      </div>
+    </div>
+  ),
+});
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
