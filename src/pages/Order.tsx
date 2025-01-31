47ae3cfa2f88af4823dcc9bdbe2306f87d160942
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-01-30 19:47:29 -0800

    add order page

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,3 +1,17 @@
+import data from '../data/data.json';
+
 export default function Order() {
-  return <div>Order</div>;
+  return (
+    <div className='bg-green h-screen  flex flex-col py-7 text-cream text-2xl md:flex-row p-5 md:p-14 '>
+      {/* left */}
+      <div className='text-'>
+        <p className='text-yellow italic'>We're located at</p>
+        <div>{data.restaurant.name}</div>
+        <div>{data.restaurant.location}</div>
+        <div>{data.restaurant.contact.phone}</div>
+      </div>
+      {/* right */}
+      <div>map</div>
+    </div>
+  );
 }
